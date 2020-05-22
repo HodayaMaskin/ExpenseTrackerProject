08331f314c4ac701be50c4d8a6b743cda3415502
Hodaya Maskin <t-homask@microsoft.com>
2020-05-22 05:56:09 +0300

    Add project files.

---
+++ Home.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp2.Views;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Window
+    {
+        NorthwindEntities _context = new NorthwindEntities();
+        public List<City> CityProperty { get; set; }
+
+        public List<City> HodayaProp { get; set; }
+
+        public List<Revenue> HodayaRevenue { get; set; }
+        public Home(NorthwindEntities context)
+        {
+           // HomeViewModel viewModel = new HomeViewModel();
+            _context = context;
+            InitializeComponent();
+            SetChartsSourceData();
+        }
+
+        private void SetChartsSourceData()
+        {
+            HodayaProp = new List<City>();
+            HodayaProp = _context.Cities.ToList();
+            HodayaProp.Add(new City() { CityId = 5, NameCity = "TelAviv" });
+            HodayaProp.Add(new City() { CityId = 5, NameCity = "Ashkelon" });
+            HodayaRevenue = _context.Revenues.ToList();
+            HodayaRevenue.Add(new Revenue() { Mounth = 1, Sum = 1000, UserId = 1 });
+            HodayaRevenue.Add(new Revenue() { Mounth = 2, Sum = 3000, UserId = 1 });
+            HodayaRevenue.Add(new Revenue() { Mounth = 3, Sum = 2500, UserId = 1 });
+            HodayaRevenue.Add(new Revenue() { Mounth = 4, Sum = 1000, UserId = 1 });
+            HodayaRevenue.Add(new Revenue() { Mounth = 5, Sum = 1800, UserId = 1 });
+            HodayaRevenue.Add(new Revenue() { Mounth = 6, Sum = 2500, UserId = 1 });
+            HodayaRevenue.Add(new Revenue() { Mounth = 7, Sum = 3400, UserId = 1 });
+            HodayaRevenue.Add(new Revenue() { Mounth = 8, Sum = 1900, UserId = 1 });
+
+            ChartSeries.ItemsSource = HodayaProp;
+            DoughnutChart.ItemsSource = HodayaRevenue;
+            column.ItemsSource = HodayaProp;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var homeWindow = new Home(_context);
+            homeWindow.Show();
+            this.Close();
+        }
+
+        private void Expense_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var expensesWindow = new Expenses();
+            expensesWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NorthwindEntities context = new NorthwindEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var listCity = from p in context.Cities select p.NameCity;
+            ObservableCollection<string> cities = new ObservableCollection<string>(listCity);
+            lstCities.DataContext = cities;
+
+            var listStatus = from p in context.Status select p.NameStatus;
+            ObservableCollection<string> status = new ObservableCollection<string>(listStatus);
+            lstStatus.DataContext = status;
+        }
+
+        private void connect_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            //if (int.Parse(personalIDTextBox.Text) != 0) 
+                if (personalIDTextBox.Text==null || personalIDTextBox.Text=="")
+                {
+                var list = from p in context.Personals select p;
+                ObservableCollection<Personal> personals = new ObservableCollection<Personal>(list);
+                Personal personal = new Personal();
+                foreach (var per in personals)
+                {
+                    if (per.UserID == int.Parse(personalIDTextBox.Text))
+                    {
+                        MessageBox.Show("personal ID is valid");
+                        //var newForm = new MainPersonal(per); //create your new form.
+                        ////var newForm = new MainPerson(int.Parse(personalIDTextBox.Text)); //create your new form.
+                        //newForm.Show(); //show the new form
+                    }
+                }
+            }
+            else
+            {
+                //
+                MessageBox.Show("personal ID is not valid");
+            }
+
+            MainPersonal newForm = new MainPersonal(); //create your new form.
+            this.Hide();
+            newForm.Show(); //show the new form
+
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            NorthwindEntities context = new NorthwindEntities();
+
+            //if (int.Parse(personalIDTextBox.Text) != 0) 
+            if (personalIDTextBox.Text != null && personalIDTextBox.Text != "")
+            {
+                var list = from p in context.Personals select p;
+                ObservableCollection<Personal> personals = new ObservableCollection<Personal>(list);
+                Personal personal = new Personal();
+                foreach (var per in personals)
+                {
+                    if (per.UserID == int.Parse(personalIDTextBox.Text))
+                    {
+                        MessageBox.Show("personal ID is exit");
+                        //var newForm = new MainPersonal(per); //create your new form.
+                        ////var newForm = new MainPerson(int.Parse(personalIDTextBox.Text)); //create your new form.
+                        //newForm.Show(); //show the new form
+                    }
+                    else
+                    {
+                        //personal.UserID
+                        //personal.FirstName
+                        //personal.LastName
+                        //personal.Gender
+                        //personal.Status
+                        //personal.CityId
+                        //personal.Age
+                        //personal.Children
+                       
+                        //context.Expenses.Add(expense);
+                        context.SaveChanges();
+                        context.SaveChanges();
+
+                        // Refresh the grids so the database generated values show up.
+                        //this.p.Items.Refresh();
+                        //this.productsDataGrid.Items.Refresh();
+                    }
+                }
+            }
+            else
+            {
+                //
+                MessageBox.Show("personal ID is not valid");
+            }
+
+            MainPersonal newForm = new MainPersonal(); //create your new form.
+            this.Hide();
+            newForm.Show(); //show the new form
+
+        }
+
+        private void RadioButton_Checked_Exist(object sender, RoutedEventArgs e)
+        {
+            radioExit.IsChecked = true;
+            radioNew.IsChecked = false;
+        }
+
+        private void Hodaya_Click(object sender, RoutedEventArgs e)
+        {
+            var homeWindow = new Home(context);
+            homeWindow.Show();
+            this.Close();
+        }
+    }
+}
